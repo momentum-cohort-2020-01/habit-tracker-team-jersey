75c8258370ce090c462475c47d100b9c5eab5165
Dane <danecodesnc@gmail.com>
2020-03-04 14:43:05 -0500

    models work

--- core/polls/models.py
+++ core/polls/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+#The daily target
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+      
+    def __str__(self):
+        return f'{self.name}'
+
+
+
+class Habit(models.Model):
+    habit = models.CharField(max_length=100)
+    habit_user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True,)
+    description = models.TextField(max_length=1000)
+    slug = models.SlufField(null=False, unique=True)
+   
+   
+    def __str__(self):
+        return f'{self.habit}'
+
+
+
+class Tracker(models.Model):
+    person = models.ForeignKey('User', on_delete=models.SET_NULL, null=True,)
+    habit_tracked = models.ForeignKey('Habit', on_delete=models.SET_NULL, null=True,)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
+    def __str__(self):
+        return f'{self.person}'
+   
+     
+ 
+
+
+
+   
+   
+   
+
+
 
 # Create your models here.
