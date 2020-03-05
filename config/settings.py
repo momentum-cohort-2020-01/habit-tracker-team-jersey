bb037e5558e27eb930d780a6570f1443e55bbec6
Dane <danecodesnc@gmail.com>
2020-03-05 13:48:52 -0500

    Working tool bar debugger

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 INTERNAL_IPS = [
 
