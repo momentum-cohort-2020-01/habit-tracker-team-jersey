0ab50d54c9d3b10300965733d696f0150651fb63
odyssic <stevenbuddenjr@gmail.com>
2020-03-04 22:07:18 -0500

    startup pages

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import User, Habit, Tracker
+
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def index(request):
+    users = User.objects.all()
+    return render(request, "core/habit_list.html", {'habits': habits})
+
+
+def daily_log(request):
+    view =
+
+
+def show_habits(request):
+    habit = Habit.objects.all()
+    return render(request, "core/habit_choices.html"
+                  {'habits': habits})
+
+
+def tracker(request):
+    tracker = Tracker.objects.all()
+    return render(request, )
