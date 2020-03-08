14787358cb194eb2e3853919166d76ae4f5928de
odyssic <stevenbuddenjr@gmail.com>
2020-03-08 12:32:04 -0400

    shuffling model names

--- polls/forms.py
+++ polls/forms.py
@@ -6,7 +6,7 @@ class HabitForm(forms.ModelForm):
 
     class Meta:
         model = Habit
-        fields = ('habit', 'goal', 'goal_units', 'timeframe')
+        fields = ('habit_name', 'action', 'goal', 'unit', 'timeframe')
 
 
 class ProgressForm(forms.ModelForm):
