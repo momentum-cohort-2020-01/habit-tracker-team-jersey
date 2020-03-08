bb2dcfb0e4d76a5f0bb92ea03682a1958b91f107
Dane <danecodesnc@gmail.com>
2020-03-07 20:21:01 -0500

    Got delete button to work

--- polls/models.py
+++ polls/models.py
@@ -24,8 +24,8 @@ class Habit(models.Model):
     timeframe = models.CharField(max_length=20, default="",)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(max_length=60, null=False, unique=True,
-                            default="")
+    slug = models.SlugField(max_length=60, null=False, unique=True, default="")
+                           
 
     def __str__(self):
         return f'{self.habit}'
